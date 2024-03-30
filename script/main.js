f240bd1ec6228d881481a19876f6706f703e55aa
gah2k0 <gah.francisco@gmail.com>
2024-03-30 19:14:59 -0300

    adding js script to show message on forms

---
+++ script/main.js
@@ -0,0 +1,8 @@
+const inputSubmitReference = document.getElementById('accept');
+
+inputSubmitReference.addEventListener("click", () => {
+    window.alert('Mensagem Enviada! Em breve entraremos em contato.')
+});
+
+
+// Adicionando ação de alert ao clicar no botão de enviar do formulário
